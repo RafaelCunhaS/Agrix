68f89a21f1cdc82af85366bde2e6032bb6301ee6
Rafael Santos <rafaelcs_94@hotmail.com>
2023-10-30 10:29:01 -0300

    feat: update Person entity, repository and service

---
+++ src/main/java/com/betrybe/agrix/models/repositories/PersonRepository.java
@@ -0,0 +1,16 @@
+package com.betrybe.agrix.models.repositories;
+
+import com.betrybe.agrix.models.entities.Person;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Person JPA repository.
+ */
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+  UserDetails findByUsername(String username);
+}
---
+++ src/main/java/com/betrybe/agrix/services/PersonService.java
@@ -0,0 +1,59 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.exceptions.PersonAlreadyExistsException;
+import com.betrybe.agrix.exceptions.PersonNotFoundException;
+import com.betrybe.agrix.models.entities.Person;
+import com.betrybe.agrix.models.repositories.PersonRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service layer class for handling persons business logic.
+ */
+@Service
+public class PersonService implements UserDetailsService {
+
+  private final PersonRepository personRepository;
+
+  @Autowired
+  public PersonService(
+      PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  /**
+   * Returns a person for a given ID.
+   */
+  public Person getPersonById(Long id) {
+    Optional<Person> person = personRepository.findById(id);
+
+    if (person.isEmpty()) {
+      throw new PersonNotFoundException();
+    }
+
+    return person.get();
+  }
+
+  /**
+   * Creates a new person.
+   */
+  public Person create(Person person) {
+    UserDetails existingPerson = this.loadUserByUsername(person.getUsername());
+    if (existingPerson != null) {
+      throw new PersonAlreadyExistsException();
+    }
+    String hashedPassword = new BCryptPasswordEncoder().encode(person.getPassword());
+    person.setPassword(hashedPassword);
+    return personRepository.save(person);
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return personRepository.findByUsername(username);
+  }
+}
